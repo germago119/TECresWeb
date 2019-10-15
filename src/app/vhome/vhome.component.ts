15580d414861324896a49b20eda631de68b6535b
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2019-10-14 20:41:45 -0600

    Validaciones de datos en Vhome

--- src/app/vhome/vhome.component.ts
+++ src/app/vhome/vhome.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-vhome',
   templateUrl: './vhome.component.html',
-  styleUrls: ['./vhome.component.sass']
+  styleUrls: ['./vhome.component.css']
 })
 export class VHomeComponent implements OnInit {
 
